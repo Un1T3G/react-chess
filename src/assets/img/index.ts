c3d99b8407ba9d5ecf5eabf7426b870d117f6b56
Un1T3G <un1t3g@gmail.com>
2023-02-07 12:02:44 +0300

    initial commit

---
+++ src/assets/img/index.ts
@@ -0,0 +1,27 @@
+import blackBishop from './bB.png'
+import blackKing from './bK.png'
+import blackKnight from './bN.png'
+import blackPawn from './bP.png'
+import blackQueen from './bQ.png'
+import blackRook from './bR.png'
+import whiteBishop from './wB.png'
+import whiteKing from './wK.png'
+import whiteKnight from './wN.png'
+import whitePawn from './wP.png'
+import whiteQueen from './wQ.png'
+import whiteRook from './wR.png'
+
+export {
+  blackBishop,
+  blackKing,
+  blackKnight,
+  blackPawn,
+  blackQueen,
+  blackRook,
+  whiteBishop,
+  whiteKing,
+  whiteKnight,
+  whitePawn,
+  whiteQueen,
+  whiteRook,
+}
